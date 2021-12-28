b7c4718ccbcc9e484838e6718d1f80ee1069bf67
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-12-28 16:03:56 +0900

    feat: material uiのテンプレートを導入

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GenericTemplate from "../templates/GenericTemplate";
+
 
 const ProjectPage: React.FC = () => {
-  return <>案件ページ</>
+  return (
+    <GenericTemplate title="商品ページ">
+      <>商品ページ内容</>
+    </GenericTemplate>
+  );
 }
 
 export default ProjectPage
